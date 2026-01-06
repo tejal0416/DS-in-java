d8766532057e91087cf1637d007866da96c9b093
tejal0416 <tejalnchaudhari@gmail.com>
2026-01-06 17:36:12 +0530

    Add method to print N x N asterisk pattern
    Implement a method to print a square pattern of asterisks.

---
+++ pattern1.c
@@ -0,0 +1,19 @@
+/*
+    For N=5 ->
+        *****
+        *****
+        *****
+        *****
+        *****
+*/ 
+class Solution {
+    public void pattern1(int n) {
+        for(int i=0;i<n;i++){
+            for (int j =0;j<n;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+    }
+}
